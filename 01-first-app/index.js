4d33b295cf19a154e3c39a03def5bbcc21cb16af
wangxiaohan <814719883@qq.com>
2018-03-08 18:24:16 +0800

    对结果进行精确度

--- 01-first-app/index.js
+++ 01-first-app/index.js
@@ -1,5 +1,3 @@
-
-
     
    var width = document.getElementById("rectangle-width"); 
    var height =document.getElementById('rectangle-height');
@@ -14,26 +12,34 @@
                     aa.innerHTML = "宽度不能为空!";
                                        
                        }
-                          
+         else{
+           aa.innerHTML = '';
+         }          
                 }
     height.onblur = function(){
        if(height.value ==''){
            bb.innerHTML = "高度不能为空!";
               
-   }
+         
+        }
+       else{
+         bb.innerHTML = '';
+       }
 
  } 
 
 
 
   calc.onclick=function(){
-      var wid =width.value;
-          var hei =height.value;
-              
-              perimeter.value=wid * 2 +hei * 2;
-                  area.value = wid * hei ;
-                      
-                      
-}
+      var x =width.value;
+      var y =height.value;
+         
+      var lenX = (x.split('.').length === 2) ? x.split('.')[1].length : 0;
+      var lenY = (y.split('.').length === 2) ? y.split('.')[1].length : 0;
 
-
+      var max = Math.max(lenX, lenY);
+     perimeter.value= (x * (10 ** max) + y * (10 ** max)) * 2 / (10 ** max);
+    area.value=  (x * (10 ** max)) * (y * (10 ** max)) / (10 ** (2 * max));
+              //perimeter.value=wid * 2 +hei * 2;
+              //  area.value = wid * hei ;
+  }              
