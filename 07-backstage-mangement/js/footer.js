093da529c60688029c26bbaeb03a487c0a666755
王晓晗 <814719883@qq.com>
2018-04-10 08:29:23 +0000

    Delete footer.js

--- 07-backstage-mangement/js/footer.js
+++
@@ -1,8 +0,0 @@
-var $footer=(function(){
-
-function show(config){
-	$(app.config.appContainer).append('<div  class="admin-app-footer"><p>2017 <a href="#">中科佰融</a></div>');
-
-}
-return {show:show};
-})();
